234bd8db2277635b01ae21261b53e1ed74a7e572
Ddhruv-IOT <ddhruvarora2@gmail.com>
2024-02-05 21:24:00 +0530

    test data

---
+++ server/data/data.js
@@ -0,0 +1,9 @@
+export const users = [
+    { id: '1', username: 'user1', password: 'password1' },
+    { id: '2', username: 'user2', password: 'password2' }
+  ];
+
+export const secretCode = [
+    { id: '1', secret: "Hello User, hope you are doing well, your code X2459L"}, 
+    { id: '2', secret: "Hello Bro, Are you all right ?, btw your code Dfjgk3L"},
+]
