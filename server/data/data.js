85af9d9179744118eb83c3ce6f72cd07f84da4b9
Ddhruv-IOT <ddhruvarora2@gmail.com>
2024-02-17 12:55:00 +0530

    added user Ddhruv

--- server/data/data.js
+++ server/data/data.js
@@ -1,9 +1,12 @@
 export const users = [
     { id: '1', username: 'user1', password: 'password1' },
-    { id: '2', username: 'user2', password: 'password2' }
+    { id: '2', username: 'user2', password: 'password2' }, 
+    { id: '3', username: 'Ddhruv', password: 'password3'}
   ];
 
 export const secretCode = [
     { id: '1', secret: "Hello User, hope you are doing well, your code X2459L"}, 
     { id: '2', secret: "Hello Bro, Are you all right ?, btw your code Dfjgk3L"},
+    { id: '3', secret: "Hello Bro, OAuth 2.0, btw your code Dfjgdek3L"},
+
 ]
